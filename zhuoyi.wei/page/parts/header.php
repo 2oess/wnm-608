676566b6a63142b7b2f7ec9010e5cb2f72099621
ZHUOYI WEI <99633093+2oess@users.noreply.github.com>
2022-04-01 14:54:05 +0800

    Add files via upload

---
+++ zhuoyi.wei/page/parts/header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#example-navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><h4>Style guide</h4></a>
+            </div>
+            <div class="collapse navbar-collapse" id="example-navbar-collapse">
+                <ul class="nav navbar-nav" style="float: right">
+                    <li class=""><a href="#"><h4>HOME</h4></a></li>
+                    <li><a href="#"><h4>SHOP</h4></a></li>
+                    <li><a href="#"><h4>NEWS</h4></a></li>
+                    <li><a href="#"><h4>ABOUT</h4></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
