dfb67e1c99c573f96968687d327bad7747e9029f
ZHUOYI WEI <99633093+2oess@users.noreply.github.com>
2022-04-27 07:10:45 -0700

    Add files via upload

---
+++ parts/navbar.php
@@ -0,0 +1,18 @@
+
+<header class="navbar">
+	<div class="container display-flex">
+		<div class="flex-none">
+			<a href="index.php"></a>
+		</div>
+		<div class="flex-stretch"></div>
+		<nav class="nav nav-flex flex-none">
+			<ul>
+				<li><a href="./admin/index.php">HOME</a></li>
+				<li><a href="product_list.php">SHOP</a></li>
+				<li><a href="./admin/users.php">ME</a></li>
+				<li><a href="about.php">ABOUT</a></li>
+				<li><a href="product_cart.php">CART</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
