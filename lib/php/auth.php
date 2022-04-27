c45ea0bc1a69e0330d2b1e04161ede38057d7eea
ZHUOYI WEI <99633093+2oess@users.noreply.github.com>
2022-04-27 07:07:56 -0700

    Add files via upload

---
+++ lib/php/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+function MYSQLIAuth(){
+	return[
+ "localhost",   //mysql host 
+"zhuoyi_2oebags",  //  mysql user name
+"99002b941002Y",  // mysql user password
+"zhuoyi_2oebags"   // mysql database name
+	];
+}
