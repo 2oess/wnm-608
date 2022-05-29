d022bbaf95136b0119a7af8861a1e25beb32adfd
ZHUOYI WEI <99633093+2oess@users.noreply.github.com>
2022-05-29 00:02:20 -0700

    Delete database.php

--- lib/php/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$dbConn =mysqli_connect("localhost", "zhuoyi_2oebags", "99002b941002Y", "zhuoyi_2oebags") or die("Unable to connect to the database ");
-mysqli_query($dbConn,'SET NAMES utf8');
-
-?>
